5b6767b53417da6800c1e0d5692dbe320f03607d
unknown <frontcatbackend@gmail.com>
2021-09-10 18:26:15 +0300

    commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 var parser = require('ua-parser-js');
-const port = 3000
+const port = process.env.PORT || 80
 
 app.get('/', (req, res) => {
   var ua = parser(req.headers['user-agent']);
